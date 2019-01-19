5da4fa64eb2e6e502cd1dc6f4d6a9bc4e71f71fd
Richard Cook <rcook@rcook.org>
2019-01-19 06:41:30 -0800

    SDL2 demo program

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,29 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
 
+import Control.Monad (unless)
+import Linear (V4(..))
+import SDL
+
 main :: IO ()
-main = putStrLn "hello world"
+main = do
+    initializeAll
+    window <- createWindow "My SDL Application" defaultWindow
+    renderer <- createRenderer window (-1) defaultRenderer
+    appLoop renderer
+
+appLoop :: Renderer -> IO ()
+appLoop renderer = do
+    events <- pollEvents
+    let eventIsQPress event =
+            case eventPayload event of
+                KeyboardEvent keyboardEvent ->
+                    keyboardEventKeyMotion keyboardEvent == Pressed &&
+                        keysymKeycode (keyboardEventKeysym keyboardEvent) == KeycodeQ
+                _ -> False
+        qPressed = any eventIsQPress events
+    rendererDrawColor renderer $= V4 0 0 255 255
+    clear renderer
+    present renderer
+    unless qPressed (appLoop renderer)
